2a14c3d9d2241b9d31e1ff74543e472812a82e22
Aditya Sonel <aditya.sonel@outlook.com>
2018-07-27 12:22:29 +0530

    LoginActivity UI Changed/PhoneVerification UI Added

---
+++ app/src/main/java/com/huntersdevs/www/gmessngr/activity/CodeVerifyActivity.java
@@ -0,0 +1,15 @@
+package com.huntersdevs.www.gmessngr.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.huntersdevs.www.gmessngr.R;
+
+public class CodeVerifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_verify);
+    }
+}
--- app/src/main/java/com/huntersdevs/www/gmessngr/activity/LoginActivity.java
+++ app/src/main/java/com/huntersdevs/www/gmessngr/activity/LoginActivity.java
@@ -7,8 +7,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -36,23 +39,25 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,
-        GoogleApiClient.ConnectionCallbacks {
+public class LoginActivity extends AppCompatActivity {
 
     private static String TAG = LoginActivity.class.getName();
 
     private Context mContext;
-    private GoogleApiClient mGoogleApiClient;
-    private static final int RC_GOOGLE_SIGN_IN = 9001;
 
     private FirebaseAuth mFirebaseAuth;
 
-    @BindView(R.id.tv_login)
-    TextView tvLogin;
     @BindView(R.id.fl_loading)
     FrameLayout flLoading;
     @BindView(R.id.lav)
     LottieAnimationView lav;
+
+    @BindView(R.id.tv_country_code)
+    TextView tvCountryCode;
+    @BindView(R.id.et_phone_number)
+    EditText etPhoneNumber;
+    @BindView(R.id.tv_next)
+    TextView tvNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,19 +68,28 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
         mFirebaseAuth = FirebaseAuth.getInstance();
 
-        GoogleSignInOptions mGoogleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+        tvNext.setEnabled(false);
+        etPhoneNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        if (mGoogleApiClient == null) {
-            try {
-                mGoogleApiClient = new GoogleApiClient.Builder(mContext)
-                        .addConnectionCallbacks(this)
-                        .addApi(Auth.GOOGLE_SIGN_IN_API, mGoogleSignInOptions)
-                        .build();
-            } catch (Exception ex) {
-                Log.d(TAG, "Exception while building GoogleApiClient: " + ex.toString());
             }
-        }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!String.valueOf(s).contains(" ") && s.length() == 10) {
+                    tvNext.setEnabled(true);
+                } else if (s.length() <= 9) {
+                    tvNext.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 
     @OnClick(R.id.tv_app_name)
@@ -83,89 +97,9 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         Util.showShortToast(mContext, getApplicationContext().getString(R.string.youareawesome));
     }
 
-    @OnClick(R.id.tv_login)
-    public void onClickTVLogin() {
-        if (!Util.isNetworkAvailable(mContext)) {
-            Util.showShortToast(mContext, getString(R.string.checknetworkconnection));
-        } else {
-            flLoading.setVisibility(View.VISIBLE);
-            tvLogin.setEnabled(false);
-            Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-            startActivityForResult(intent, RC_GOOGLE_SIGN_IN);
-        }
+    @OnClick(R.id.tv_next)
+    public void onClickBtnNext() {
+        startActivity(new Intent(LoginActivity.this, CodeVerifyActivity.class));
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_GOOGLE_SIGN_IN) {
-            GoogleSignInResult mGoogleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (mGoogleSignInResult.isSuccess()) {
-                GoogleSignInAccount mGoogleSignInAccount = mGoogleSignInResult.getSignInAccount();
-                if (mGoogleSignInAccount != null) {
-                    firebaseAuthByGoogle(mGoogleSignInAccount);
-                }
-            }
-        }
-    }
-
-    private void firebaseAuthByGoogle(GoogleSignInAccount mGoogleSignInAccount) {
-        AuthCredential authCredential = GoogleAuthProvider.getCredential(mGoogleSignInAccount.getIdToken(), null);
-        mFirebaseAuth.signInWithCredential(authCredential)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            flLoading.setVisibility(View.GONE);
-                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                            finish();
-                        } else {
-                            tvLogin.setEnabled(true);
-                            mFirebaseAuth.signOut();
-                            flLoading.setVisibility(View.GONE);
-                            Util.showLongToast(mContext, getString(R.string.somethingwentwrong));
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                tvLogin.setEnabled(true);
-                mFirebaseAuth.signOut();
-                flLoading.setVisibility(View.GONE);
-                Util.showLongToast(mContext, getString(R.string.somethingwentwrong));
-                Log.d(TAG, "onFailure listenr: " + e.toString());
-            }
-        });
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (mGoogleApiClient == null || !mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.connect();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.disconnect();
-        }
-    }
 }
